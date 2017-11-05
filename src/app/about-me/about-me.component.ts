6a254d0801295becc4f462c9edca8ed3707ebb82
Michael <mgh0010@auburn.edu>
2017-11-05 14:41:50 -0600

    picture change

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-me.component.css']
 })
 export class AboutMeComponent implements OnInit {
-	myImageUrl = '/assets/media/myself1.jpg';
+	myImageUrl1 = '/assets/media/myself1.jpg';
+	myImageUrl2 = '/assets/media/myself3.jpg';
 
   constructor() { }
 
