067666e9881f18e1e5ea975d6d778c3bbfafc27b
Michael <mgh0010@auburn.edu>
2017-11-07 20:06:29 -0600

    new

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 	home4DawgsUrl = 'https://home4dawgs.herokuapp.com'
+	phfUrl = 'https://github.com/mgh0010/phf'
 
   constructor() { }
 
