afb9c7f588e12b55ce08bf6b918b9dff72405805
Michael <mgh0010@auburn.edu>
2017-11-19 15:21:06 -0600

    new css stuff like p size large now

--- src/app/contact-me/contact-me.component.ts
+++ src/app/contact-me/contact-me.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-me.component.css']
 })
 export class ContactMeComponent implements OnInit {
+	mailUrl = 'mailto:mghharrison@gmail.com';
+	telUrl = 'tel:+1-205-516-3702'
 
   constructor() { }
 
