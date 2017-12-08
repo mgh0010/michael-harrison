6014cca4b635185218cbe30a9f0a4e562efcff80
Michael <mgh0010@auburn.edu>
2017-12-08 15:10:39 -0600

    added groupme bot to projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -9,6 +9,7 @@ export class ProjectsComponent implements OnInit {
 	home4DawgsUrl = 'https://home4dawgs.herokuapp.com'
 	phfUrl = 'https://github.com/mgh0010/phf'
 	whyDoThatUrl = 'https://whydothat.herokuapp.com/'
+	groupMeBotUrl = 'https://github.com/mgh0010/lil-cute-bot'
 
   constructor() { }
 
